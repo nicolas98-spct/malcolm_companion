b1e220b0143899059167f41a5c593a01a89f5f66
José Francisco Ordóñez Urgilés <ordonezurgilesjosefrancisco@gmail.com>
2026-01-23 09:22:11 -0500

    Create personajes and perfil components

---
+++ src/app/pages/personajes/personajes.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personajes',
+  imports: [],
+  templateUrl: './personajes.html',
+  styleUrl: './personajes.scss',
+})
+export class Personajes {
+
+}
